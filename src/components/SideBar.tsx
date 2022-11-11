020139c74618a99136b7f6a9d01497e1ced7de6f
joel <joel@reboundstudios.com>
2022-11-11 10:38:47 -0500

    Updated Sidebar & Navbar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,23 +1,27 @@
+import Link from "next/link";
 import { FC, ReactNode } from "react";
 import { TbDeviceAnalytics, TbChartDots2, TbSoccerField } from "react-icons/tb";
 
 export const SideBar: FC = () => {
   return (
-    <div className="fixed top-0 left-0 h-screen w-16 m-0
-      flex flex-col bg-gray-900 text-white shadow-lg
-      hover:w-48 transition-all duration-500 ease-in-out">
+    <div className="group fixed top-0 left-0 h-screen w-16 m-0
+      flex flex-col bg-slate-700 text-white shadow-lg
+      hover:w-48 items-start transition-all duration-500 ease-in-out">
       
-      <SideBarIcon icon={<TbDeviceAnalytics size={32} />} />
-      <SideBarIcon icon={<TbChartDots2 size={32} />} />
-      <SideBarIcon icon={<TbSoccerField size={32} />}/>
+      <SideBarIcon icon={<TbDeviceAnalytics size={32} />} href="/" text="Dashboard" />
+      <SideBarIcon icon={<TbChartDots2 size={32} />} href="/" text="Graph" />
+      <SideBarIcon icon={<TbSoccerField size={32} />} href="/" text="Field" />
     </div>
   );
 };
 
-export const SideBarIcon = ({ icon }: any) => {
+export const SideBarIcon = (props: any) => {
   return (
-    <div className="sidebar-icon">
-      {icon}
-    </div>
+    <Link href={props.href}>
+      <div className="sidebar-icon">
+        {props.icon}
+        <span className="sidebar-tooltip">{props.text}</span>
+      </div>
+    </Link>
   );
 };
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,11 @@ import { FC } from 'react';
 export const NavBar: FC = () => {
   return (
     <nav className="h-14 w-full bg-white py-0 px-4 border-b-4">
+      <Link href={"/"}>
+        <div className="h-full w-fit my-auto mx-4 text-5xl font-bold text-black hover:cursor-pointer">
+          Baseball Toolbox
+        </div>
+      </Link>
       <div className="">
       </div>
     </nav>
